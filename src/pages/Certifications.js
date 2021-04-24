fb7557082af9fa3492c1332f7bf92e2a8ee9bee9
Jonathan <oldschoolspaceempire@gmail.com>
2021-04-24 13:35:45 -0500

    added a second Carousel for the Certifications page

--- src/pages/Certifications.js
+++ src/pages/Certifications.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import CarouselTwo from '../components/CarouselTwo'
 import Hero from '../components/Hero'
 
 export default function Certifications(props) {
   return (
     <div>
       <Hero title={props.title} />
+      <CarouselTwo />
     </div>
   )
 }
